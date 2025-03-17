88fa6f5a437d37930a97ee597cb5ed3121f52d4e
Meghdad <meghdadhsz@outlook.com>
2025-03-17 19:56:57 +0330

    complete till FAQ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-mh-lightBlue/10`}
       >
         <Header />
         {children}
